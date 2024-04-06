3004dacf1f37529b4e30fa7b2b2395d44a73d494
Radajaaj <1vitor.camargo@gmail.com>
2024-04-06 15:15:26 -0300

    Alteracoes mutacao + funcInicializa

---
+++ funcInicializa.py
@@ -0,0 +1,26 @@
+#Gera aleatoriamente uma população de n indivíduos
+#Llimites de (𝑥 ∈ [−2.5, 2.5]; 𝑦 ∈ [−2.5, 2.5]);
+
+#aparentemente, no python, listas podem ser passadas de uma função para outra sem problemas (passagem por referência)
+import random
+
+def inicializa_genes(tamPopulacao, tamGene, geracao1): #recebe a lista por endereço
+    
+    for i in range(tamPopulacao):
+        #Primeiro, vamos criar uma string vazia para armazenar nosso numero binario
+        numero = ""
+        #Depois, geramos seus bits
+        for i in range(tamGene):
+            numero += str(random.randint(0, 1)) #geramos um int entre 0 e 1, e convertemos pra string
+        geracao1.append(numero) #colocamos nosso gene no final da lista
+
+
+#int main()
+tamPopulacao = 100
+tamGene = 16        #(8 bits para X + 8 bits para Y)
+listaGenes = [] #definimos a lista aonde será armazenado cada indivíduo da geração (em binário)
+
+inicializa_genes(tamPopulacao, tamGene, listaGenes) #incializamos a função com os parâmetros acima
+
+for i in listaGenes: #printamos a lista
+    print(i)
