a94ecd78ab69e4384c325a75ff9e7118185ff414
Radajaaj <1vitor.camargo@gmail.com>
2024-04-06 15:24:58 -0300

    Inicializacao agora faz o .

--- funcInicializa.py
+++ funcInicializa.py
@@ -6,12 +6,18 @@ import random
 
 def inicializa_genes(tamPopulacao, tamGene, geracao1): #recebe a lista por endereço
     
+
     for i in range(tamPopulacao):
+        
         #Primeiro, vamos criar uma string vazia para armazenar nosso numero binario
         numero = ""
         #Depois, geramos seus bits
-        for i in range(tamGene):
-            numero += str(random.randint(0, 1)) #geramos um int entre 0 e 1, e convertemos pra string
+        
+        for i in range(tamGene+2):
+            if i == 3 or i == ((tamGene/2) + 4):
+                numero += "."
+            else:
+                numero += str(random.randint(0, 1)) #geramos um int entre 0 e 1, e convertemos pra string
         geracao1.append(numero) #colocamos nosso gene no final da lista
 
 
@@ -22,5 +28,6 @@ listaGenes = [] #definimos a lista aonde será armazenado cada indivíduo da ger
 
 inicializa_genes(tamPopulacao, tamGene, listaGenes) #incializamos a função com os parâmetros acima
 
+print("Sua pop inicial é:")
 for i in listaGenes: #printamos a lista
     print(i)
